739005f26173fd5bb20dec7ec9ca9b901ab26371
mementis <menasivak@gmail.com>
2021-01-15 15:41:47 +0100

    populate reg form 1

--- src/test/java/pagemethods/RegisterAccountMethods.java
+++ src/test/java/pagemethods/RegisterAccountMethods.java
@@ -1,5 +1,25 @@
 package pagemethods;
 
-public class RegisterAccountMethods {
+import org.openqa.selenium.WebDriver;
+
+import pageobjects.RegisterAccountObjects;
+
+public class RegisterAccountMethods extends RegisterAccountObjects {
+	
+	WebDriver driver;
+	
+	public RegisterAccountMethods(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void populateFirstName() throws InterruptedException {
+		Thread.sleep(1000);
+		driver.findElement(firstName).sendKeys(firstNameValue);
+	}
+	
+	public void populateLastName() throws InterruptedException {
+		Thread.sleep(1000);
+		driver.findElement(lastName).sendKeys(lastNameValue);
+	}
 
 }
--- src/test/java/pageobjects/RegisterAccountObjects.java
+++ src/test/java/pageobjects/RegisterAccountObjects.java
@@ -1,5 +1,12 @@
 package pageobjects;
 
+import org.openqa.selenium.By;
+
 public class RegisterAccountObjects {
+	
+	public static By firstName = By.id("FirstName");
+	public static String firstNameValue = "Mara";
+	public static By lastName = By.id("LastName");
+	public static String lastNameValue = "Popara";
 
 }
